36eb548756169cba8852ce3e2cbc5107d57d4b13
Randall Upham <randall.upham@genesys.com>
2021-03-05 15:59:45 -0500

    Express, problem 2: Hello World in response to a get request

--- myApp.js
+++ myApp.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 
-console.log("Hello World");
+function rootGetHandler(req, res) {
+  var responseString = 'Hello World';
+  console.log('Request = ' + req);
+  console.log('Response will be "' + responseString + '"');
+  res.send(responseString);
+}
 
-
-
+app.get('/', rootGetHandler);
 
 
 
