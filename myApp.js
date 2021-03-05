a0938ae83ca172659aeb0b7a257d65ef8be72162
Randall Upham <randall.upham@genesys.com>
2021-03-05 17:02:03 -0500

    Express, problem 6:  using environment variables

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var path = require('path');
+var dotenv = require('dotenv');
 var app = express();
+
+dotenv.config();
 
 function rootGetHandler(req, res) {
   var indexPath = path.join(__dirname, '/views/index.html');
@@ -8,7 +11,13 @@ function rootGetHandler(req, res) {
 }
 
 function jsonGetHandler(req, res) {
-  var responseObj = { "message": "Hello json" };
+  var responseString = "Hello json";
+  console.log(process.env.MESSAGE_STYLE);
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    console.log('Converting to upper case');
+    responseString = responseString.toUpperCase();
+  }
+  var responseObj = { "message": responseString };
   res.json(responseObj);
 }
 
@@ -16,6 +25,7 @@ app.get('/', rootGetHandler);
 app.get('/json', jsonGetHandler);
 app.use('/public', express.static(__dirname + '/public'));
 
+console.log(process.env.MESSAGE_STYLE);
 
 
 
